911a71db394996a907ab1e7a18087ef546a9d603
JulianaCadavid <juliana.cadavidr@udea.edu.co>
2022-02-24 15:43:02 -0500

    General products added

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,6 +2,8 @@ import Client from "@/pages/Client";
 import Login from "@/pages/Login/Login";
 import { Route, Routes } from "react-router-dom";
 import ProductForm from "@/pages/Product";
+import GeneralProduct from "@/pages/Product/GeneralProduct";
+import Products from "../Product";
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/client" element={<Client />} />
       <Route path="/product" element={<ProductForm />} />
+      <Route path="/general" element={<GeneralProduct/>}/>
     </Routes>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer ({client, location, address, telephone }){
+    return(
+        <div className="bg-sky-100 text-center">
+            <h1>{client}</h1>
+            <h1>{location}</h1>
+            <h1>{address}</h1>
+            <h1>{telephone}</h1>
+        </div>
+    )
+}
+
+export default Footer
