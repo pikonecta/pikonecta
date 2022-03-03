17880564401d8225d2bf98427bb4ee7ed3651252
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-03-03 10:17:24 -0500

    Added account context

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,14 +2,17 @@ import Login from "@/pages/Login";
 import Client from "@/pages/Client";
 import { Route, Routes } from "react-router-dom";
 import ProductForm from "@/pages/Product";
+import { Account } from "../Account";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/client" element={<Client />} />
-      <Route path="/product" element={<ProductForm />} />
-    </Routes>
+    <Account>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/client" element={<Client />} />
+        <Route path="/product" element={<ProductForm />} />
+      </Routes>
+    </Account>
   );
 }
 
