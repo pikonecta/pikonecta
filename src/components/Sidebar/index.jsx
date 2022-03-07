d3467f6bf7770702e1b051a0f8c5549d8dcf0411
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-03-07 14:12:37 -0500

    size fixed

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -5,7 +5,7 @@ function Sidebar({ className }) {
   return (
     <div
       className={cn(
-        "px-4 py-10 bg-sidebar-color border-r-2 border-gray-300 h-screen flex justify-between flex-col",
+        "px-4 py-10 bg-sidebar-color border-r-2 border-gray-300 h-full flex justify-between flex-col",
         className
       )}
     >
