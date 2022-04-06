0591898a3b1c70e9e4dd292eaf2807eedb83d989
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-04-06 18:40:38 -0500

    unused file

--- src/utils/urlToFileObject.js
+++
@@ -1,8 +0,0 @@
-const convertUrlToFileObject = async (url) => {
-  const res = await fetch(url);
-  const blob = await res.blob();
-  const file = new File([blob], "logo.png", { type: "image/png" });
-  return file;
-};
-
-export default convertUrlToFileObject;
