40df3903ed672f3be147c13d9d9f76451dcecba3
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-04-06 19:01:30 -0500

    getTenant and updateTenant added

--- src/utils/apiManager.js
+++ src/utils/apiManager.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { v4 as uuidv4 } from "uuid";
-import  encodedLogo  from "@/utils/encodedLogo";
+import encodedLogo from "@/utils/encodedLogo.js";
 
 const createTenant = async (data, logo) => {
   const encodeLogo = await encodedLogo(logo);
@@ -24,43 +24,48 @@ const createTenant = async (data, logo) => {
     CITY: data.city,
     DEPARTMENT: data.department,
   };
-  await axios.post("/api/tenants", req, {
+  const res = await axios.post("/api/tenants", req, {
     Authorization: import.meta.env.VITE_ADMIN_TOKEN,
   });
+  return res;
 };
 
-const updateTenant = async (data, logo) => {
-  const encodeLogo = await encodedLogo(logo);
-
+const updateTenant = async (data, setLogo, logo) => {
   const req = {
     id: data.id,
-    COMPANY_NAME: data.companyName,
+    ADDRESS: data.companyAddress,
+    CITY: data.city,
+    COUNTRY: data.country,
+    DEPARTMENT: data.department,
+    NIT: data.NIT,
     COMPANY_EMAIL: data.companyEmail,
+    COMPANY_NAME: data.companyName,
     COMPANY_PHONE: data.companyPhone,
-    COMPANY_ACTIVITY: data.companyActivity,
+    COMPANY_ACTIVITY: data.companyActivity || "",
     REPRESENTATIVE_EMAIL: data.clientEmail,
     REPRESENTATIVE_NAME: data.clientName,
     REPRESENTATIVE_PHONE: data.clientPhone,
-    NIT: data.NIT,
-    LOGO: {
+    ITS_ACTIVE: true,
+  };
+
+  if (setLogo) {
+    const encodeLogo = await encodedLogo(logo);
+    req.NEW_LOGO = {
       encodedImage: encodeLogo,
       extension: logo.name.split(".").pop(),
-    },
-    ADDRESS: data.companyAddress,
-    COUNTRY: data.country,
-    CITY: data.city,
-    DEPARTMENT: data.department,
-    ITS_ACTIVE: true
-  };
-  await axios.put(`/api/tenants/tenant`, req, {
+    };
+  } else {
+    req.CURRENT_LOGO = logo.src;
+  }
+  const res = await axios.put(`/api/tenants/tenant`, req, {
     Authorization: import.meta.env.VITE_ADMIN_TOKEN,
   });
-
+  return res;
 };
 
 const getTenant = async (id) => {
-  const res = await axios.get(`/api/tenants/tenant?id=${id}`)
-  return res.data; 
+  const res = await axios.get(`/api/tenants/tenant?id=${id}`);
+  return res.data;
 };
 
 export { createTenant, getTenant, updateTenant };
