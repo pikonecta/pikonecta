d359769b33271040648d37545b83086fd0e25b3d
Juan Manuel Grajales Urquijo <juan.grajalesu@gmail.com>
2022-04-18 21:56:35 -0500

    resizing style component

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -2,7 +2,7 @@ function Loader() {
   const circleCommonClasses = "h-2.5 w-2.5 bg-current rounded-full";
 
   return (
-    <div className="flex h-screen justify-center items-center w-screen bg-white">
+    <div className="flex h-screen justify-center items-center bg-white">
       <div className={`${circleCommonClasses} mr-1 animate-bounce`} />
       <div className={`${circleCommonClasses} mr-1 animate-bounce200`} />
       <div className={`${circleCommonClasses} animate-bounce400`} />
